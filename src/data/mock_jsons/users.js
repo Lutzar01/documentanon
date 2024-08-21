05ef1f51956887b496fd3eb1afe5333c8c020bcf
Lutzar <47092133+Lutzar01@users.noreply.github.com>
2024-08-20 21:48:18 -0700

    MockData with display
    created .js with some user mock data in src\data\mock_jsons\user.js
    removed placeholder code from app function
    updated app.js with ListUsers function

---
+++ src/data/mock_jsons/users.js
@@ -0,0 +1,32 @@
+export const usersMockData = [
+  {
+    id: 1,
+    name: "Adam",
+    age: 32,
+    email: "adam@mail.com"
+  },
+  {
+    id: 2,
+    name: "Mada",
+    age: 33,
+    email: "mada@mail.com"
+  },
+  {
+    id: 3,
+    name: "Dama",
+    age: 34,
+    email: "dama@mail.com"
+  },
+  {
+    id: 4,
+    name: "Adma",
+    age: 35,
+    email: "adma@mail.com"
+  },
+  {
+    id: 5,
+    name: "Mdaa",
+    age: 36,
+    email: "mdaa@mail.com"
+  }
+]
